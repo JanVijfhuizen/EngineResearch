7d021fb20b16e16e2b6e2b25c48a01763f32b8bf
Jan <jan.vijfhuizen@outlook.com>
2022-09-29 21:47:00 +0200

    [Add] Iterator and view.

---
+++ JEngine/Include/Iterator.h
@@ -0,0 +1,60 @@
+#pragma once
+
+namespace je
+{
+	// Standard iterator that can be used to allow for for loops in arrays/vectors and similar data structures.
+	template <typename T>
+	struct Iterator final
+	{
+		T* data = nullptr;
+		size_t length = 0;
+		size_t index = 0;
+
+		T& operator*() const;
+		T& operator->() const;
+
+		const Iterator& operator++();
+		Iterator operator++(int);
+
+		friend bool operator==(const Iterator& a, const Iterator& b)
+		{
+			return a.index == b.index;
+		}
+
+		friend bool operator!= (const Iterator& a, const Iterator& b)
+		{
+			return !(a == b);
+		}
+	};
+
+	template <typename T>
+	T& Iterator<T>::operator*() const
+	{
+		assert(data);
+		assert(index <= length);
+		return data[index];
+	}
+
+	template <typename T>
+	T& Iterator<T>::operator->() const
+	{
+		assert(data);
+		assert(index <= length);
+		return data[index];
+	}
+
+	template <typename T>
+	const Iterator<T>& Iterator<T>::operator++()
+	{
+		++index;
+		return *this;
+	}
+
+	template <typename T>
+	Iterator<T> Iterator<T>::operator++(int)
+	{
+		Iterator temp(data, length, index);
+		++index;
+		return temp;
+	}
+}
---
+++ JEngine/Include/View.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "Iterator.h"
+
+namespace je
+{
+	// Allows for overloading different data structures as a simplified and generalized view.
+	// Also works for single objects.
+	template <typename T>
+	struct View
+	{
+		// ReSharper disable once CppNonExplicitConvertingConstructor
+		View(T& instance);
+		View(void* data, size_t length);
+
+		[[nodiscard]] T& operator [](size_t index);
+		[[nodiscard]] size_t GetLength() const;
+
+		virtual Iterator<T> begin() const;
+		virtual Iterator<T> end() const;
+
+	private:
+		T* _data = nullptr;
+		size_t _length = SIZE_MAX;
+	};
+
+	template <typename T>
+	View<T>::View(T& instance) : _data(&instance), _length(1)
+	{
+
+	}
+
+	template <typename T>
+	View<T>::View(void* data, const size_t length) : _data(static_cast<T*>(data)), _length(length)
+	{
+
+	}
+
+	template <typename T>
+	T& View<T>::operator[](size_t index)
+	{
+		assert(_length > index);
+		return _data[index];
+	}
+
+	template <typename T>
+	size_t View<T>::GetLength() const
+	{
+		return _length;
+	}
+
+	template <typename T>
+	Iterator<T> View<T>::begin() const
+	{
+		Iterator<T> it{};
+		it.length = _length;
+		it.data = _data;
+		return it;
+	}
+
+	template <typename T>
+	Iterator<T> View<T>::end() const
+	{
+		Iterator<T> it{};
+		it.length = _length;
+		it.index = _length;
+		it.data = _data;
+		return it;
+	}
+}
