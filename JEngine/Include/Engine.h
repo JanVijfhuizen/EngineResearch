48a987f6315545d5b36d21c9397cfb38f5515208
Jan <jan.vijfhuizen@outlook.com>
2022-10-03 13:54:26 +0200

    [Add] Engine foundation and arena definition.

---
+++ JEngine/Include/Engine.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+#include "Jlb/Arena.h"
+
+namespace je
+{
+	class Engine
+	{
+		friend struct Initializer;
+
+	public:
+		struct CreateInfo final
+		{
+			size_t persistentArenaSize = 4096;
+			size_t tempArenaSize = 4096;
+			size_t dumpArenaSize = 4096;
+
+			[[nodiscard]] size_t GetMemorySpaceRequired() const;
+		};
+
+		Engine(const CreateInfo& info = {});
+		virtual ~Engine();
+
+		[[nodiscard]] size_t Run();
+
+	protected:
+		struct Initializer final
+		{
+			friend Engine;
+
+			template <typename T>
+			void AddModule() const;
+
+		private:
+			Engine* _engine = nullptr;
+
+			explicit Initializer(Engine& engine);
+		};
+
+		virtual void DefineAdditionalModules(const Initializer& initializer) = 0;
+
+	private:
+		void* _memory;
+		Arena _persistentArena;
+		Arena _tempArena;
+		Arena _dumpArena;
+
+		template <typename T>
+		void AddModule();
+	};
+
+	template <typename T>
+	void Engine::Initializer::AddModule() const
+	{
+		_engine->AddModule<T>();
+	}
+
+	template <typename T>
+	void Engine::AddModule()
+	{
+	}
+}
---
+++ JEngine/Src/Engine.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "Engine.h"
+
+namespace je
+{
+	size_t Engine::CreateInfo::GetMemorySpaceRequired() const
+	{
+		return persistentArenaSize + tempArenaSize + dumpArenaSize;
+	}
+
+	Engine::Engine(const CreateInfo& info) :
+		_memory(malloc(info.GetMemorySpaceRequired())),
+		_persistentArena(_memory, info.persistentArenaSize),
+		_tempArena(static_cast<unsigned char*>(_memory) + info.persistentArenaSize, info.tempArenaSize),
+		_dumpArena(static_cast<unsigned char*>(_memory) + info.persistentArenaSize + info.tempArenaSize, info.dumpArenaSize)
+	{
+	}
+
+	Engine::~Engine()
+	{
+		free(_memory);
+	}
+
+	size_t Engine::Run()
+	{
+		const Initializer initializer{ *this };
+		DefineAdditionalModules(initializer);
+		return EXIT_SUCCESS;
+	}
+
+	Engine::Initializer::Initializer(Engine& engine) : _engine(&engine)
+	{
+
+	}
+}
