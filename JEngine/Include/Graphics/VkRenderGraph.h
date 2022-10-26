8df8012a7c514125aadc7fc3b41f71cfb460a69b
Jan <jan.vijfhuizen@outlook.com>
2022-10-26 13:11:16 +0200

    [Remove] Old render graph code.

--- JEngine/Include/Graphics/VkRenderGraph.h
+++ JEngine/Include/Graphics/VkRenderGraph.h
@@ -1,25 +1,9 @@
 ﻿#pragma once
-#include "Jlb/Array.h"
-#include "Jlb/StringView.h"
-#include "Jlb/View.h"
 
 namespace je::vk
 {
-	struct RenderGraphResource final
-	{
-		StringView name;
-	};
-
-	struct RenderGraphNode
-	{
-		[[nodiscard]] virtual Array<RenderGraphResource> DefineInputs(Arena& arena);
-		[[nodiscard]] virtual Array<RenderGraphResource> DefineOutputs(Arena& arena);
-	};
-
 	class RenderGraph final
 	{
-	public:
-		RenderGraph(Arena& arena, Arena& tempArena, const View<RenderGraphNode*>& nodes, const View<RenderGraphResource>& externalResources);
-		~RenderGraph();
+	
 	};
 }
