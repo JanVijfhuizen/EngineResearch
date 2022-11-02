468c504c1e9417516ab0efd4c401fa342da3e17d
Jan <jan.vijfhuizen@outlook.com>
2022-11-02 10:41:55 +0100

    [Add] Pool.

---
+++ Jlb/Include/Jlb/Pool.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "Array.h"
+
+namespace je
+{
+	class Arena;
+
+	template <typename T>
+	class Pool final
+	{
+		struct Node;
+
+	public:
+		struct Instance final
+		{
+			friend Pool;
+
+			operator T*() const;
+
+		private:
+			Node* _node;
+		};
+
+		Pool(Arena& arena, size_t size);
+
+		[[nodiscard]] Instance Get();
+		void Put(const Instance& instance);
+
+	private:
+		struct Node final
+		{
+			T value{};
+			Node* next = nullptr;
+		};
+		
+		Array<Node> _nodes{};
+		Node* _open = nullptr;
+	};
+
+	template <typename T>
+	Pool<T>::Instance::operator T*() const
+	{
+		return &_node->value;
+	}
+
+	template <typename T>
+	Pool<T>::Pool(Arena& arena, size_t size) : _nodes(arena, size)
+	{
+		for (size_t i = size - 1; i > 0; --i)
+			_nodes[i - 1].next = &_nodes[i];
+		_open = &_nodes[0];
+	}
+
+	template <typename T>
+	typename Pool<T>::Instance Pool<T>::Get()
+	{
+		Node* current = _open;
+		_open = _open->next;
+		current->next = nullptr;
+		return { current };
+	}
+
+	template <typename T>
+	void Pool<T>::Put(const Instance& instance)
+	{
+		instance._node->next = _open;
+		_open = instance._node;
+	}
+}
